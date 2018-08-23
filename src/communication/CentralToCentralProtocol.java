d45c653e82106c2b37b8f8107fb87e93efa87b0b
Joao <joaopaulofsilvaa@gmail.com>
2018-08-23 00:22:49 -0300

    Criada a classe que auxiliará na comunicação entre centrais.

---
+++ src/communication/CentralToCentralProtocol.java
@@ -0,0 +1,54 @@
+package communication;
+
+import rescuecore2.worldmodel.EntityID;
+
+public class CentralToCentralProtocol extends AbstractMessageProtocol {
+	private final static int CHANNEL = 2;
+	private final static String TYPE = "C2C";
+	private final static int CODE = 3;
+	private char centerDestiny;
+	private EntityID eventID;
+	private EntityID eventPosition;
+	private int detail_1;
+	private int detail_2;	
+	
+	public CentralToCentralProtocol(String[] msgReceived) {
+		super(CHANNEL, msgReceived); 
+		this.centerDestiny = msgReceived[5].charAt(0);
+		this.eventID = new EntityID(Integer.parseInt(msgReceived[6]));
+		this.eventPosition = new EntityID(Integer.parseInt(msgReceived[7]));		
+		this.detail_1 = Integer.parseInt(msgReceived[8]);
+		this.detail_2 = Integer.parseInt(msgReceived[9]);
+	}
+
+	public CentralToCentralProtocol(char center, int time, EntityID senderId, String details) {
+		super(CHANNEL, TYPE, center, time, senderId, CODE, details);
+		String detailsSplitted[] = details.split(" ");
+		this.centerDestiny = detailsSplitted[0].charAt(0);
+		this.eventID = new EntityID(Integer.parseInt(detailsSplitted[1]));
+		this.eventPosition = new EntityID(Integer.parseInt(detailsSplitted[2]));		
+		this.detail_1 = Integer.parseInt(detailsSplitted[3]);
+		this.detail_2 = Integer.parseInt(detailsSplitted[4]);
+	}
+
+	public char getCenterDestiny() {
+		return centerDestiny;
+	}
+
+	public EntityID getEventID() {
+		return eventID;
+	}
+
+	public EntityID getEventPosition() {
+		return eventPosition;
+	}
+
+	public int getDetail_1() {
+		return detail_1;
+	}
+
+	public int getDetail_2() {
+		return detail_2;
+	}
+	
+}
